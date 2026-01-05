de05258c86a0efd3f5d737e7bd5efa6faf9d3060
Dat Nguyen <mauthanhdat1908@gmail.com>
2026-01-05 13:12:09 +0700

    Update database

--- topzone-db.sql
+++ topzone-db.sql
@@ -1,543 +1,112 @@
---
--- PostgreSQL database dump
---
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-\restrict radu75JoeCxv4zzriVZcQWh52Tz7nEay1XiiyeQv8umS5iyh6SU3ADgQPAVmRaf
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- Dumped from database version 18.1
--- Dumped by pg_dump version 18.1
+CREATE TABLE user_roles (
+    user_id INT NOT NULL
+        REFERENCES users(id)
+        ON DELETE CASCADE,
 
--- Started on 2025-12-30 23:32:25
+    role_id INT NOT NULL
+        REFERENCES roles(id)
+        ON DELETE CASCADE,
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+    PRIMARY KEY (user_id, role_id)
+);
 
---
--- TOC entry 863 (class 1247 OID 16404)
--- Name: order_status; Type: TYPE; Schema: public; Owner: postgres
---
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    slug VARCHAR(150) UNIQUE NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TYPE public.order_status AS ENUM (
-    'PENDING',
-    'PAID',
-    'SHIPPED',
-    'DELIVERED',
-    'CANCELLED'
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    origin_name VARCHAR(255) NOT NULL,
+    stored_name VARCHAR(255) NOT NULL,
+    mime_type VARCHAR(100) NOT NULL,
+    image_size BIGINT NOT NULL,
+    image_url TEXT NOT NULL,
+    uploaded_by INT REFERENCES users(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+
+    category_id INT 
+        REFERENCES categories(id) 
+        ON DELETE SET NULL,
+
+    name VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT,
+    price NUMERIC(12,2) NOT NULL,
+
+    discount NUMERIC(5,2) DEFAULT 0
+        CHECK (discount >= 0 AND discount <= 100),
+
+    is_active BOOLEAN DEFAULT TRUE,
+    is_archive BOOLEAN DEFAULT FALSE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product_variants (
+    id SERIAL PRIMARY KEY,
+
+    product_id INT NOT NULL
+        REFERENCES products(id)
+        ON DELETE CASCADE,
+
+    size VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+
+    price NUMERIC(12,2),
+
+    is_active BOOLEAN DEFAULT TRUE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (product_id, size, color)
 );
 
 
-ALTER TYPE public.order_status OWNER TO postgres;
+CREATE TABLE product_images (
+    product_id INT NOT NULL
+        REFERENCES products(id)
+        ON DELETE CASCADE,
 
---
--- TOC entry 866 (class 1247 OID 16416)
--- Name: payment_method; Type: TYPE; Schema: public; Owner: postgres
---
+    image_id INT NOT NULL
+        REFERENCES images(id)
+        ON DELETE CASCADE,
 
-CREATE TYPE public.payment_method AS ENUM (
-    'CREDIT_CARD',
-    'BANK_TRANSFER',
-    'E_WALLET',
-    'CASH_ON_DELIVERY'
+    is_primary BOOLEAN DEFAULT FALSE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (product_id, image_id)
 );
 
-
-ALTER TYPE public.payment_method OWNER TO postgres;
-
---
--- TOC entry 869 (class 1247 OID 16426)
--- Name: payment_status; Type: TYPE; Schema: public; Owner: postgres
---
-
-CREATE TYPE public.payment_status AS ENUM (
-    'SUCCESS',
-    'FAILED',
-    'PENDING'
-);
-
-
-ALTER TYPE public.payment_status OWNER TO postgres;
-
---
--- TOC entry 872 (class 1247 OID 16434)
--- Name: warranty_status; Type: TYPE; Schema: public; Owner: postgres
---
-
-CREATE TYPE public.warranty_status AS ENUM (
-    'REQUESTED',
-    'APPROVED',
-    'REJECTED',
-    'COMPLETED'
-);
-
-
-ALTER TYPE public.warranty_status OWNER TO postgres;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- TOC entry 224 (class 1259 OID 16664)
--- Name: categories; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.categories (
-    id integer NOT NULL,
-    name character varying(100) NOT NULL,
-    slug character varying(150) NOT NULL,
-    description text,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.categories OWNER TO postgres;
-
---
--- TOC entry 223 (class 1259 OID 16663)
--- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.categories_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.categories_id_seq OWNER TO postgres;
-
---
--- TOC entry 5103 (class 0 OID 0)
--- Dependencies: 223
--- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.categories_id_seq OWNED BY public.categories.id;
-
-
---
--- TOC entry 222 (class 1259 OID 16643)
--- Name: images; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.images (
-    id integer NOT NULL,
-    origin_name character varying(255) NOT NULL,
-    stored_name character varying(255) NOT NULL,
-    mime_type character varying(100) NOT NULL,
-    image_size bigint NOT NULL,
-    image_url text NOT NULL,
-    uploaded_by integer,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.images OWNER TO postgres;
-
---
--- TOC entry 221 (class 1259 OID 16642)
--- Name: images_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.images_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.images_id_seq OWNER TO postgres;
-
---
--- TOC entry 5104 (class 0 OID 0)
--- Dependencies: 221
--- Name: images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.images_id_seq OWNED BY public.images.id;
-
-
---
--- TOC entry 227 (class 1259 OID 16711)
--- Name: product_images; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.product_images (
-    product_id integer NOT NULL,
-    image_id integer NOT NULL,
-    sort_order integer DEFAULT 0,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.product_images OWNER TO postgres;
-
---
--- TOC entry 226 (class 1259 OID 16680)
--- Name: products; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.products (
-    id integer NOT NULL,
-    category_id integer,
-    name character varying(255) NOT NULL,
-    slug character varying(255) NOT NULL,
-    description text,
-    price numeric(12,2) NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    discount numeric(5,2) DEFAULT 0,
-    file_size bigint,
-    color character varying(50),
-    is_active boolean DEFAULT true,
-    is_archive boolean DEFAULT false,
-    CONSTRAINT chk_product_status CHECK ((NOT ((is_active = true) AND (is_archive = true)))),
-    CONSTRAINT products_discount_check CHECK (((discount >= (0)::numeric) AND (discount <= (100)::numeric)))
-);
-
-
-ALTER TABLE public.products OWNER TO postgres;
-
---
--- TOC entry 225 (class 1259 OID 16679)
--- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.products_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.products_id_seq OWNER TO postgres;
-
---
--- TOC entry 5105 (class 0 OID 0)
--- Dependencies: 225
--- Name: products_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.products_id_seq OWNED BY public.products.id;
-
-
---
--- TOC entry 229 (class 1259 OID 16739)
--- Name: roles; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.roles (
-    id integer NOT NULL,
-    name character varying(50) NOT NULL,
-    description text,
-    is_active boolean DEFAULT true,
-    is_archive boolean DEFAULT false,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.roles OWNER TO postgres;
-
---
--- TOC entry 228 (class 1259 OID 16738)
--- Name: roles_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.roles_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.roles_id_seq OWNER TO postgres;
-
---
--- TOC entry 5106 (class 0 OID 0)
--- Dependencies: 228
--- Name: roles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.roles_id_seq OWNED BY public.roles.id;
-
-
---
--- TOC entry 230 (class 1259 OID 16755)
--- Name: user_roles; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.user_roles (
-    user_id integer NOT NULL,
-    role_id integer NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.user_roles OWNER TO postgres;
-
---
--- TOC entry 220 (class 1259 OID 16626)
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    email character varying(255) NOT NULL,
-    password_hash character varying(255) NOT NULL,
-    full_name character varying(255) NOT NULL,
-    phone character varying(20),
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
-
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- TOC entry 219 (class 1259 OID 16625)
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
-
---
--- TOC entry 5107 (class 0 OID 0)
--- Dependencies: 219
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- TOC entry 4902 (class 2604 OID 16667)
--- Name: categories id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.categories_id_seq'::regclass);
-
-
---
--- TOC entry 4899 (class 2604 OID 16646)
--- Name: images id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.images ALTER COLUMN id SET DEFAULT nextval('public.images_id_seq'::regclass);
-
-
---
--- TOC entry 4905 (class 2604 OID 16683)
--- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_id_seq'::regclass);
-
-
---
--- TOC entry 4914 (class 2604 OID 16742)
--- Name: roles id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.roles ALTER COLUMN id SET DEFAULT nextval('public.roles_id_seq'::regclass);
-
-
---
--- TOC entry 4896 (class 2604 OID 16629)
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- TOC entry 4930 (class 2606 OID 16676)
--- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4932 (class 2606 OID 16678)
--- Name: categories categories_slug_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_slug_key UNIQUE (slug);
-
-
---
--- TOC entry 4928 (class 2606 OID 16657)
--- Name: images images_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.images
-    ADD CONSTRAINT images_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4938 (class 2606 OID 16720)
--- Name: product_images product_images_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.product_images
-    ADD CONSTRAINT product_images_pkey PRIMARY KEY (product_id, image_id);
-
-
---
--- TOC entry 4934 (class 2606 OID 16695)
--- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4936 (class 2606 OID 16697)
--- Name: products products_slug_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_slug_key UNIQUE (slug);
-
-
---
--- TOC entry 4940 (class 2606 OID 16754)
--- Name: roles roles_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.roles
-    ADD CONSTRAINT roles_name_key UNIQUE (name);
-
-
---
--- TOC entry 4942 (class 2606 OID 16752)
--- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.roles
-    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4944 (class 2606 OID 16763)
--- Name: user_roles user_roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_roles
-    ADD CONSTRAINT user_roles_pkey PRIMARY KEY (user_id, role_id);
-
-
---
--- TOC entry 4924 (class 2606 OID 16641)
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- TOC entry 4926 (class 2606 OID 16639)
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4947 (class 2606 OID 16726)
--- Name: product_images fk_image; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.product_images
-    ADD CONSTRAINT fk_image FOREIGN KEY (image_id) REFERENCES public.images(id) ON DELETE SET NULL;
-
-
---
--- TOC entry 4948 (class 2606 OID 16721)
--- Name: product_images fk_product; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.product_images
-    ADD CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES public.products(id) ON DELETE SET NULL;
-
-
---
--- TOC entry 4949 (class 2606 OID 16769)
--- Name: user_roles fk_role; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_roles
-    ADD CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES public.roles(id) ON DELETE SET NULL;
-
-
---
--- TOC entry 4950 (class 2606 OID 16764)
--- Name: user_roles fk_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.user_roles
-    ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE SET NULL;
-
-
---
--- TOC entry 4945 (class 2606 OID 16658)
--- Name: images images_uploaded_by_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.images
-    ADD CONSTRAINT images_uploaded_by_fkey FOREIGN KEY (uploaded_by) REFERENCES public.users(id) ON DELETE SET NULL;
-
-
---
--- TOC entry 4946 (class 2606 OID 16698)
--- Name: products products_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id) ON DELETE SET NULL;
-
-
--- Completed on 2025-12-30 23:32:25
-
---
--- PostgreSQL database dump complete
---
-
-\unrestrict radu75JoeCxv4zzriVZcQWh52Tz7nEay1XiiyeQv8umS5iyh6SU3ADgQPAVmRaf
 
